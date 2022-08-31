ee323a3faf41cdbbed4fb3584c5429414899ac88
sayantan bhaumik <iamsaayantan@gmail.com>
2022-08-31 15:09:11 +0530

    added ChartDataLabels plugin

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import {Bar,Pie} from 'react-chartjs-2';
 
+
+//for chartlabels
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
 // without this import chart will not render
 import Chart from 'chart.js/auto';
 
 
 
 const BarChart = ()=>{
+   
     return(
 
         <>
-            <Bar
+            {/* <Bar
+
+
+
+            //first curly brace dynamic javascript content
+            //2nd curly brace javascript object
+            data={{
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+            label: 'sample label',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+
+            }}
+            height={400}
+            width={600}
+            
+            options={{
+                maintainAspectRatio:false
+            }}
+            plugins={[ChartDataLabels]}
+
+             /> */}
+            
+             <Pie
 
             //first curly brace dynamic javascript content
             //2nd curly brace javascript object
@@ -44,46 +90,10 @@ const BarChart = ()=>{
             options={{
                 maintainAspectRatio:false
             }}
+              plugins={[ChartDataLabels]}
 
              />
             
-             {/* <Pie
-
-            //first curly brace dynamic javascript content
-            //2nd curly brace javascript object
-            data={{
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-            label: 'sample label',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-
-            }}
-            height={400}
-            width={600}
-            options={{
-                maintainAspectRatio:false
-            }}
-
-             />
-             */}
         </>
     );
 }
@@ -92,3 +102,4 @@ const BarChart = ()=>{
 
 
 export default BarChart;
+
